fc772f79d8757fd884c30f80c28cfe11c98acca4
space-cap <hong.git@yahoo.com>
2024-05-15 01:19:55 +0900

    for 문을 이용해서 text 반복 출력
    for 문을 이용해서 text 반복 출력

--- stateful_widget/lib/main.dart
+++ stateful_widget/lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      numbers.add(numbers.length);
     });
   }
 
@@ -34,10 +34,7 @@ class _MyAppState extends State<MyApp> {
                 'Click Count',
                 style: TextStyle(fontSize: 60),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 40),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
