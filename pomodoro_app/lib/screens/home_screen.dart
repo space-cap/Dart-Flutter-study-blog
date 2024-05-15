6245e21cbaa4194de4b368b409449cd1bf6e8c47
space-cap <hong.git@yahoo.com>
2024-05-15 16:40:55 +0900

    Initial commit
    Initial commit

---
+++ pomodoro_app/lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.red),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.green),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.blue),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
