645713b2ce1080b177b33f8a1422589ede3b10ad
space-cap <hong.git@yahoo.com>
2024-05-16 16:40:21 +0900

    fromJson
    fromJson

--- webtoon_app/lib/services/api_service.dart
+++ webtoon_app/lib/services/api_service.dart
@@ -1,22 +1,27 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon_app/models/webtoon_model.dart';
 
 class ApiService {
   static const String baseUrl =
       "https://webtoon-crawler.nomadcoders.workers.dev";
   static const String today = "today";
 
-  static void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url); // 비동기로 정보를 받아온다.
+
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        print(webtoon);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      return;
+
+      return webtoonInstances;
     }
+
     throw Error();
   }
 }
