f232fcaf5a99a894d801aa8700fc588547bd91d3
space-cap <hong.git@yahoo.com>
2024-05-16 20:34:42 +0900

    Hero 추가
    Hero 추가

--- webtoon_app/lib/widgets/webtoon_widget.dart
+++ webtoon_app/lib/widgets/webtoon_widget.dart
@@ -30,29 +30,32 @@ class Webtoon extends StatelessWidget {
       },
       child: Column(
         children: [
-          Container(
-            width: 200,
-            // 모서리 라운드 처리.
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15,
-                  offset: const Offset(10, 10),
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ],
-            ),
-            child: Image.network(
-              thumb,
-              headers: const {
-                'Referer': 'https://comic.naver.com',
-              },
-              errorBuilder: (context, error, stackTrace) {
-                print(error);
-                return const Text('error');
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 200,
+              // 모서리 라운드 처리.
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: const Offset(10, 10),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  'Referer': 'https://comic.naver.com',
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  print(error);
+                  return const Text('error');
+                },
+              ),
             ),
           ),
           const SizedBox(
