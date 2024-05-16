fa81eeac44b614c2d2e60d3ff62639b9eb0be029
space-cap <hong.git@yahoo.com>
2024-05-16 20:21:00 +0900

    Detail Screen
    Detail Screen 이미지 추가

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -26,6 +26,43 @@ class DetailScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                // 모서리 라운드 처리.
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ],
+                ),
+                child: Image.network(
+                  thumb,
+                  headers: const {
+                    'Referer': 'https://comic.naver.com',
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    print(error);
+                    return const Text('error');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
