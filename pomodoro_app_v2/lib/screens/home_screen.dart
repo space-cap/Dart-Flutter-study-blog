7944f28d19cb3f40dc2d7bf542b523e3f6154d91
space-cap <hong.git@yahoo.com>
2024-05-17 19:24:37 +0900

    Initial commit
    Initial commit

---
+++ pomodoro_app_v2/lib/screens/home_screen.dart
@@ -0,0 +1,200 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+extension DurationFormatter on Duration {
+  /// Returns a day, hour, minute, second string representation of this `Duration`.
+  ///
+  ///
+  /// Returns a string with days, hours, minutes, and seconds in the
+  /// following format: `dd:HH:MM:SS`. For example,
+  ///
+  ///   var d = new Duration(days:19, hours:22, minutes:33);
+  ///    d.dayHourMinuteSecondFormatted();  // "19:22:33:00"
+  String dayHourMinuteSecondFormatted() {
+    toString();
+    return [
+      inDays,
+      inHours.remainder(24),
+      inMinutes.remainder(60),
+      inSeconds.remainder(60)
+    ].map((seg) {
+      return seg.toString().padLeft(2, '0');
+    }).join(':');
+  }
+
+  String minuteSecondFormatted() {
+    toString();
+    return [inMinutes.remainder(60), inSeconds.remainder(60)].map((seg) {
+      return seg.toString().padLeft(2, '0');
+    }).join(':');
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSecond = twentyFiveMinutes;
+  bool isRunning = false;
+  int totalPomodoros = 0;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSecond == 0) {
+      setState(() {
+        totalPomodoros = totalPomodoros + 1;
+        isRunning = false;
+        totalSecond = twentyFiveMinutes;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSecond = totalSecond - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onResetPressed() {
+    if (totalSecond != twentyFiveMinutes) {
+      timer.cancel();
+      setState(() {
+        isRunning = false;
+        totalSecond = twentyFiveMinutes;
+      });
+    }
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+
+    //var minuteSecondFormat = duration.minuteSecondFormatted();
+    //print(minuteSecondFormat);
+
+    //print(
+    //   "${duration.inMinutes.remainder(60)}:${(duration.inSeconds.remainder(60)).toString().padLeft(2, '0')}");
+
+    //return "${duration.inMinutes.remainder(60)}:${(duration.inSeconds.remainder(60)).toString().padLeft(2, '0')}";
+    //return minuteSecondFormat;
+
+    print(duration.toString().substring(2, 7));
+
+    return duration.toString().substring(2, 7);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                format(totalSecond),
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(isRunning
+                        ? Icons.pause_circle_outline
+                        : Icons.play_circle_outline),
+                  ),
+                  // reset 버튼 추가.
+                  IconButton(
+                    iconSize: 60,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? () {} : onResetPressed,
+                    icon: Icon(isRunning
+                        ? Icons.lock_reset_outlined
+                        : Icons.restore_outlined),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pomodoros',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color,
+                            ),
+                          ),
+                          Text(
+                            '$totalPomodoros',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge!
+                                  .color,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
