af27e32c3098d18f15554bc27c8131977aa87108
space-cap <hong.git@yahoo.com>
2024-05-17 11:37:48 +0900

    detail info
    detail info 추가

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -84,6 +84,26 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Text(
+                    snapshot.data!.about,
+                    style: const TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          ),
         ],
       ),
     );
