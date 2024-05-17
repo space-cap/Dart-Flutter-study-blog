6b5291d498459363f5488edbe52e23291e0ae811
space-cap <hong.git@yahoo.com>
2024-05-17 11:56:26 +0900

    episodes
    episode 타이틀 추가.

--- webtoon_app/lib/screens/detail_screen.dart
+++ webtoon_app/lib/screens/detail_screen.dart
@@ -118,6 +118,21 @@ class _DetailScreenState extends State<DetailScreen> {
               return const Text("...");
             },
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+              future: episodes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      for (var episode in snapshot.data!) Text(episode.title),
+                    ],
+                  );
+                }
+                return Container();
+              }),
         ],
       ),
     );
